719262ef27a434bf62bcf54f58b50db7cd72e9ac
Saad Anees <saadanees1@gmail.com>
2025-04-10 15:53:28 +0400

    Added egg model and script

---
+++ Assets/Scripts/EggTap.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class EggTap : MonoBehaviour
+{
+    public GameObject crackedEggPrefab;
+    public GameObject character;
+    public int tapsToCrack = 4;
+
+    public Texture[] crackStages; // Assign 4 textures in the Inspector
+    private int tapCount = 0;
+
+    private Renderer eggRenderer;
+
+    private void Start()
+    {
+        eggRenderer = GetComponent<Renderer>();
+    }
+
+    private void OnMouseDown()
+    {
+        tapCount++;
+
+        if (tapCount < tapsToCrack)
+        {
+            Debug.Log("Crack sound / effect here!");
+
+            // Swap texture based on tap
+            if (tapCount - 1 < crackStages.Length && eggRenderer != null)
+            {
+                eggRenderer.material.mainTexture = crackStages[tapCount - 1];
+            }
+        }
+        else
+        {
+            CrackOpen();
+        }
+    }
+
+    void CrackOpen()
+    {
+        Instantiate(crackedEggPrefab, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+        if (character != null)
+        {
+            character.SetActive(true);
+        }
+
+        Debug.Log("Egg cracked open!");
+    }
+}
